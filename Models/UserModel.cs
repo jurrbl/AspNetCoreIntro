45eec039f408831575857779f197ef6ddc24d6e4
Belba Jurgen <124259718+jurrbl@users.noreply.github.com>
2024-11-29 12:30:06 +0100

    Aggiunto Model UserModel, With Parameters Name, Surname, DOB, POB, ID

---
+++ Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AspNetCoreIntro.Models
+{
+    public class UserModel(int id, string name, string surname, DateTime dob, string pob)
+    {
+
+        public int Id { get; set; } = id;
+        public string Name { get; set; } = name;
+        public string Surname { get; set; } = surname;
+        public DateTime DateOfBirth { get; set; } = dob;
+        public string Pob { get; set; } = pob;
+
+    }
+
+}
