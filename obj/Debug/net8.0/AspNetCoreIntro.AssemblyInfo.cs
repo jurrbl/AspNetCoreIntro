b476ee90232fffc46b144fe0bda636ca40ac7685
Belba Jurgen <124259718+jurrbl@users.noreply.github.com>
2024-12-06 13:22:50 +0100

    Started talking about Dep Injection

--- obj/Debug/net8.0/AspNetCoreIntro.AssemblyInfo.cs
+++ obj/Debug/net8.0/AspNetCoreIntro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCoreIntro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45eec039f408831575857779f197ef6ddc24d6e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d144ae15141dd5b1b4ae9ea7a281bdd9d758ebb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspNetCoreIntro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspNetCoreIntro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
